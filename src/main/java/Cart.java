dadd01c65b81cefe31b29b278ca822e22504a1e4
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:42:19 +0800

    fixed: when shipperName not exist should throw IllegalArgumentException

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -2,13 +2,17 @@ import java.util.HashMap;
 
 public class Cart {
 
-    private HashMap<String, Shipper> shippers = new HashMap<>() {{
+    private final HashMap<String, Shipper> shippers = new HashMap<>() {{
         put("black cat", new BlackCat());
         put("hsinchu", new Hsinchu());
         put("post office", new PostOffice());
     }};
 
     public double shippingFee(String shipperName, Product product) {
-        return shippers.get(shipperName).calculateFee(product);
+        if (shippers.containsKey(shipperName)) {
+
+            return shippers.get(shipperName).calculateFee(product);
+        }
+        throw new IllegalArgumentException("shipper not exist");
     }
 }
