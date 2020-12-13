de51b89552494799c058aed0ec44fac5f1c83761
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:35:36 +0800

    refactoring: extract variables when hsinchu and postOffice

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -6,10 +6,12 @@ public class Cart {
                 Shipper shipper = new BlackCat();
                 return shipper.calculateFee(product);
             case "hsinchu": {
-                return new Hsinchu().calculateFee(product);
+                shipper = new Hsinchu();
+                return shipper.calculateFee(product);
             }
             case "post office": {
-                return new PostOffice().calculateFee(product);
+                shipper = new PostOffice();
+                return shipper.calculateFee(product);
             }
             default:
                 throw new IllegalArgumentException("shipper not exist");
