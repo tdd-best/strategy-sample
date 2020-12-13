20ed7ea00892a9b8e05767ba1561a31ef106a47f
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:32:54 +0800

    refactoring: make Hsinchu and PostOffice implements Shipper interface

--- src/main/java/Hsinchu.java
+++ src/main/java/Hsinchu.java
@@ -1,8 +1,8 @@
-public class Hsinchu {
+public class Hsinchu implements Shipper {
     public Hsinchu() {
     }
 
-    double calculateFee(Product product) {
+    public double calculateFee(Product product) {
         if (product.getLength() > 100 || product.getWidth() > 100 || product.getHeight() > 100) {
             return product.getSize() * 0.00002 * 1100 + 500;
         } else {
--- src/main/java/PostOffice.java
+++ src/main/java/PostOffice.java
@@ -1,8 +1,8 @@
-public class PostOffice {
+public class PostOffice implements Shipper {
     public PostOffice() {
     }
 
-    double calculateFee(Product product) {
+    public double calculateFee(Product product) {
         double feeByWeight = 80 + product.getWeight() * 10;
         double feeBySize = product.getSize() * 0.00002 * 1100;
         return Math.min(feeByWeight, feeBySize);
