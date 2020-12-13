d937aab669e68d538d491cbab38e99bfe2c1e9a9
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:28:55 +0800

    refactoring: rename calculateFee()

--- src/main/java/BlackCat.java
+++ src/main/java/BlackCat.java
@@ -2,7 +2,7 @@ public class BlackCat {
     public BlackCat() {
     }
 
-    double calculateFeeByBlackCat(Product product) {
+    double calculateFee(Product product) {
         if (product.getWeight() > 20) {
             return 500;
         } else {
--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -4,7 +4,7 @@ public class Cart {
     public double shippingFee(String shipper, Product product) {
         switch (shipper) {
             case "black cat":
-                return blackCat.calculateFeeByBlackCat(product);
+                return blackCat.calculateFee(product);
             case "hsinchu": {
                 if (product.getLength() > 100 || product.getWidth() > 100 || product.getHeight() > 100) {
                     return product.getSize() * 0.00002 * 1100 + 500;
