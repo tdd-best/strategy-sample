5df9300b97683c61f7a4dac5bea560b6288f0b27
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:41:10 +0800

    refactoring: use HashMap instead of switch cases

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,26 +1,14 @@
+import java.util.HashMap;
+
 public class Cart {
 
-    public double shippingFee(String shipperName, Product product) {
-        return getShipper(shipperName).calculateFee(product);
-    }
+    private HashMap<String, Shipper> shippers = new HashMap<>() {{
+        put("black cat", new BlackCat());
+        put("hsinchu", new Hsinchu());
+        put("post office", new PostOffice());
+    }};
 
-    private Shipper getShipper(String shipperName) {
-        Shipper shipper;
-        switch (shipperName) {
-            case "black cat":
-                shipper = new BlackCat();
-                break;
-            case "hsinchu": {
-                shipper = new Hsinchu();
-                break;
-            }
-            case "post office": {
-                shipper = new PostOffice();
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("shipper not exist");
-        }
-        return shipper;
+    public double shippingFee(String shipperName, Product product) {
+        return shippers.get(shipperName).calculateFee(product);
     }
 }
