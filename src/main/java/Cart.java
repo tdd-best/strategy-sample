378b5014b72e3bde53960f08fad4e0b32f0748e6
Joey Chen <jay.hatelove@gmail.com>
2020-12-13 16:23:33 +0800

    refactoring: use switch cases instead of if/else if statement

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,25 +1,28 @@
 public class Cart {
     public double shippingFee(String shipper, double length, double width, double height, double weight) {
-        if (shipper.equals("black cat")) {
-            if (weight > 20) {
-                return 500;
-            } else {
-                return 100 + weight * 10;
+        switch (shipper) {
+            case "black cat":
+                if (weight > 20) {
+                    return 500;
+                } else {
+                    return 100 + weight * 10;
+                }
+            case "hsinchu": {
+                double size = length * width * height;
+                if (length > 100 || width > 100 || height > 100) {
+                    return size * 0.00002 * 1100 + 500;
+                } else {
+                    return size * 0.00002 * 1200;
+                }
             }
-        } else if (shipper.equals("hsinchu")) {
-            double size = length * width * height;
-            if (length > 100 || width > 100 || height > 100) {
-                return size * 0.00002 * 1100 + 500;
-            } else {
-                return size * 0.00002 * 1200;
+            case "post office": {
+                double feeByWeight = 80 + weight * 10;
+                double size = length * width * height;
+                double feeBySize = size * 0.00002 * 1100;
+                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
             }
-        } else if (shipper.equals("post office")) {
-            double feeByWeight = 80 + weight * 10;
-            double size = length * width * height;
-            double feeBySize = size * 0.00002 * 1100;
-            return feeByWeight < feeBySize ? feeByWeight : feeBySize;
-        } else {
-            throw new IllegalArgumentException("shipper not exist");
+            default:
+                throw new IllegalArgumentException("shipper not exist");
         }
     }
 }
